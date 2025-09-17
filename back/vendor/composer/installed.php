7c2eccb79f8e82f47115c5ee233717876364a59b
Astrit Marleku <astritmarleku1993@gmail.com>
2025-09-17 16:23:43 +0200

    Small Change

---
+++ back/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '59f2fb2f328841a4fa28bf9fe1f9f376c529150a',
+        'type' => 'template',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '59f2fb2f328841a4fa28bf9fe1f9f376c529150a',
+            'type' => 'template',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bower-asset/jquery' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => 'fde1f76e2799dd877c176abde0ec836553246991',
+            'type' => 'bower-asset',
+            'install_path' => __DIR__ . '/../bower-asset/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
